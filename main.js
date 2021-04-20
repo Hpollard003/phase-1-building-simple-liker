a3bb8f31371cb6c5b71c703dd1b3de5186472abc
Hpollard003 <hpollard003@gmail.com>
2021-04-20 15:56:54 -0400

    Done.

--- main.js
+++ main.js
@@ -1,9 +1,37 @@
-// Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+// Defining text characters for the empty and full hearts for you to use later.// Your JavaScript code goes here!
+const EMPTY_HEART = '🤍'
+const FULL_HEART = '💙'
 
-// Your JavaScript code goes here!
+const error = document.getElementById('modal')
+error.setAttribute('class','hidden')
 
+const heartTransplant = document.getElementsByClassName('like-glyph')
+for (i = 0; i < heartTransplant.length; i++) {
+  heartTransplant[i].addEventListener('click', ui)
+}
+
+function ui(event) {
+  const swap = event.target
+  mimicServerCall()
+  .then(()=>{
+    if (swap.innerText === EMPTY_HEART){
+      swap.innerText = FULL_HEART;
+      swap.className = '';
+    }
+    else {
+      swap.className = '';
+      swap.innerText = EMPTY_HEART;
+}  })
+.catch((err) => {
+  
+  error.removeAttribute("class")
+  let h2 = error.querySelector("h2")
+  h2.innerText = "Weird that didnt work. One sec. Okay give her a go!!"
+  setTimeout(() => {
+    error.setAttribute('class', 'hidden')
+  }, 4000)
+})  
+}
 
 
 
